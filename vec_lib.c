939cd3485e4677acb834fe73e9a414794db8fef2
PLATONOV Sergey <sergey.platonov@etu.hesge.ch>
2019-05-30 17:42:19 +0200

    first commit of container files

---
+++ vec_lib.c
@@ -0,0 +1,12 @@
+/*======================================
+ * Galaxy Simulation using Quad-Trees
+ * HEPIA 2018-2019 ITI 1
+ * Sergey PLATONOV
+ * vec_lib.c
+ * Library for basic vector manipulations
+======================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
